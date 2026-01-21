b9057fd341b38ffa0a5c9923051183c498d52d82
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2026-01-22 08:57:26 +1100

    local extension configuration

---
+++ extension/extension_config_local.cmake
@@ -0,0 +1,18 @@
+duckdb_extension_load(ducklake
+    GIT_URL https://github.com/duckdb/ducklake
+    GIT_TAG 415a9ebdbd73db50a8c6ba703eb733ed16bcf33a
+)
+duckdb_extension_load(httpfs
+    GIT_URL https://github.com/duckdb/duckdb-httpfs
+    GIT_TAG 13e18b3c9f3810334f5972b76a3acc247b28e537
+)
+duckdb_extension_load(mysql_scanner
+    GIT_URL https://github.com/duckdb/duckdb-mysql
+    GIT_TAG e2760cb11f13a12ce74e8c798c07c48deb97a577
+    SUBMODULES database-connector
+)
+duckdb_extension_load(postgres_scanner
+    GIT_URL https://github.com/duckdb/duckdb-postgres
+    GIT_TAG c89234f0b1985f4ee0f52f16e742a1ab2d4ae4f0
+    SUBMODULES database-connector
+)
